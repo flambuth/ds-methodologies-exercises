a3b22cdb1c9f0badd040e5b2fd584e48c533c4c3
Fredrick Lambuth <flambuth@gmail.com>
2019-10-11 10:35:55 -0500

    with official review

--- regression/explore.py
+++ regression/explore.py
@@ -1,4 +1,47 @@
 #exploring the depths of Powershell blue
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import warnings
+warnings.filterwarnings("ignore")
+
+import env
+import wrangle
+import split_scale
+
+#1
 # Write a function, plot_variable_pairs(dataframe) that plots all of the pairwise 
 # relationships along with the regression line for each pair.
+
+def plot_variable_pairs(dataframe):
+    train, test = split_scale.split_my_data(dataframe)
+    g = sns.PairGrid(train)
+    g.map_diag(plt.hist)
+    g.map_offdiag(plt.scatter);
+
+#2
+#Write a function, months_to_years(tenure_months, df) that returns your dataframe 
+#with a new feature tenure_years, in complete years as a customer.
+
+def months_to_years(tenure_months, df):
+    df['tenure_years'] = df.tenure/12
+    df['tenure_years'] = df.tenure_years.astype(int)
+    return df
+#3
+# Write a function, plot_categorical_and_continous_vars(categorical_var, continuous_var, 
+# df), that outputs 3 different plots for plotting a categorical variable with a 
+# continuous variable, e.g. tenure_years with total_charges. For ideas on effective 
+# ways to visualize categorical with continuous: https://datavizcatalogue.com/. You 
+# can then look into seaborn and matplotlib documentation for ways to create plots.
+
+def plot_categorical_and_continous_vars(categorical_var, continuous_var):
+    
+
+#############
+wrangle.wrangle_telco()
--- regression/split_scale.py
+++ regression/split_scale.py
@@ -22,19 +22,19 @@ import env
 
 
 #Use our home-baked data cleaning function
-df = wrangle_grades.wrangle_grades()
+#df = wrangle_grades.wrangle_grades()
 
 #Split up the data into a 80/20 split. Training our model with .8 of the data, and then testing the model
 #on the remaining .2
 #The random state dictates the randomness of how each row is assigned in this 80/20 split.
 #If you use the same random state, the members of test and train will be the same
-train, test = train_test_split(df, train_size = .80, random_state = 123)
+#train, test = train_test_split(df, train_size = .80, random_state = 123)
 
 #Create the scaling object that will transform our train&test data.
 #This transformation will just be a scale operation, so a change in the range of the data,
 #but the data will keep it's order and relation to one another. They're just widened or narrowed in range.
 #This scaler object before it is assigned. The .fit() method that takes the trained data.
-scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
+#scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
 #This operation is cramming the .fit into the assignment
 
 
@@ -47,9 +47,9 @@ scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
 
 # transform train, uses the pandas dataframe constructor. The parameters look like it takes what we used to make
 # 'train_scaled data', and then an argument that defines the column names
-train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+#train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
 # transform test, repeat process for making train_scaled, but using 'test' in the boxes to fill.
-test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+#test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
 
 ########################
 
@@ -57,30 +57,30 @@ test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).
 
 #Our block of data, that i had to use with the in-module funciton because mine wont work when it's imported.
 #Why i dont know. I will bother me on long grey mornings as I grieve about it.
-telco_data = wrangle.wrangle_telco()
+#telco_data = wrangle.wrangle_telco()
 
 #X, our predictor is all the columns except for total chages
-X = telco_data.drop(columns=['customer_id', 'total_charges'])
+#X = telco_data.drop(columns=['customer_id', 'total_charges'])
 #y, is our target variable, the total charges of each customer(row). We want to predict that based on the 
 #data we have in X
-y = telco_data['total_charges']
-y = pd.DataFrame(y)
+#y = telco_data['total_charges']
+#y = pd.DataFrame(y)
 
 #Split each up into 80/20 train/test groups. They both have teh same random seed, and they each have the indexing
 #from their original DataFrame, 'telco_data'.
-trainX, testX = train_test_split(X, train_size = .80, random_state = 123)
-trainy, testy = train_test_split(y, train_size = .80, random_state = 123)
+# trainX, testX = train_test_split(X, train_size = .80, random_state = 123)
+# trainy, testy = train_test_split(y, train_size = .80, random_state = 123)
 
 # standard_scaler()
-scalerX = StandardScaler(copy=True, with_mean=True, with_std=True).fit(trainX)
-scalery = StandardScaler(copy=True, with_mean=True, with_std=True).fit(trainy)
+# scalerX = StandardScaler(copy=True, with_mean=True, with_std=True).fit(trainX)
+# scalery = StandardScaler(copy=True, with_mean=True, with_std=True).fit(trainy)
 
 
-trainX_scaled = pd.DataFrame(scalerX.transform(trainX), columns=trainX.columns.values).set_index(trainX.index.values)
-testX_scaled = pd.DataFrame(scalerX.transform(testX), columns=testX.columns.values).set_index(testX.index.values)
+# trainX_scaled = pd.DataFrame(scalerX.transform(trainX), columns=trainX.columns.values).set_index(trainX.index.values)
+# testX_scaled = pd.DataFrame(scalerX.transform(testX), columns=testX.columns.values).set_index(testX.index.values)
 
-trainy_scaled = pd.DataFrame(scalery.transform(trainy), columns=trainy.columns.values).set_index(trainy.index.values)
-testy_scaled = pd.DataFrame(scalery.transform(testy), columns=testy.columns.values).set_index(testy.index.values)
+# trainy_scaled = pd.DataFrame(scalery.transform(trainy), columns=trainy.columns.values).set_index(trainy.index.values)
+# testy_scaled = pd.DataFrame(scalery.transform(testy), columns=testy.columns.values).set_index(testy.index.values)
 
 # scale_inverse()
 ################
@@ -89,27 +89,27 @@ testy_scaled = pd.DataFrame(scalery.transform(testy), columns=testy.columns.valu
 
 
 # uniform_scaler()
-scalerX = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(trainX)
-scalery = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(trainy)
+# scalerX = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(trainX)
+# scalery = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=123, copy=True).fit(trainy)
 
-trainX_scaled = pd.DataFrame(scalerX.transform(trainX), columns=trainX.columns.values).set_index([trainX.index.values])
-trainy_scaled = pd.DataFrame(scalery.transform(trainy), columns=trainy.columns.values).set_index([trainy.index.values])
-
-
-testy_scaled = pd.DataFrame(scaleryX.transform(testX), columns=testX.columns.values).set_index([testX.index.values])
-testy_scaled = pd.DataFrame(scaleryy.transform(testy), columns=testy.columns.values).set_index([testy.index.values])
+# trainX_scaled = pd.DataFrame(scalerX.transform(trainX), columns=trainX.columns.values).set_index([trainX.index.values])
+# trainy_scaled = pd.DataFrame(scalery.transform(trainy), columns=trainy.columns.values).set_index([trainy.index.values])
 
 
-# gaussian_scaler()
-scalerX = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(trainX)
-scalery = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(trainy)
-# apply to train
-train_scaledX = pd.DataFrame(scalerX.transform(train), columns=trainX.columns.values).set_index([trainX.index.values])
-train_scaledy = pd.DataFrame(scalery.transform(train), columns=trainy.columns.values).set_index([trainy.index.values])
+# testy_scaled = pd.DataFrame(scalerX.transform(testX), columns=testX.columns.values).set_index([testX.index.values])
+# testy_scaled = pd.DataFrame(scalery.transform(testy), columns=testy.columns.values).set_index([testy.index.values])
 
-# apply to test
-test_scaledX = pd.DataFrame(scalerX.transform(test), columns=testX.columns.values).set_index([testX.index.values])
-test_scaledy = pd.DataFrame(scalery.transform(test), columns=testy.columns.values).set_index([testy.index.values])
+
+# # gaussian_scaler()
+# scalerX = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(trainX)
+# scalery = PowerTransformer(method='yeo-johnson', standardize=False, copy=True).fit(trainy)
+# # apply to train
+# train_scaledX = pd.DataFrame(scalerX.transform(train), columns=trainX.columns.values).set_index([trainX.index.values])
+# train_scaledy = pd.DataFrame(scalery.transform(train), columns=trainy.columns.values).set_index([trainy.index.values])
+
+# # apply to test
+# test_scaledX = pd.DataFrame(scalerX.transform(test), columns=testX.columns.values).set_index([testX.index.values])
+# test_scaledy = pd.DataFrame(scalery.transform(test), columns=testy.columns.values).set_index([testy.index.values])
 
 
 # min_max_scaler()
@@ -128,7 +128,9 @@ def split_my_data(data, train_ratio=.80, seed=123):
     return train_test_split(data, train_size = train_ratio, random_state = seed)
 
 
-
+#The process is: Make the scaler object, and fit it while you're there.
+#Apply the scaling object to the train and test set, which is assigned to a new variable.
+#Function gives back those scaled dataframes, along with the scaling object fitted to the training dataframe.
 def standard_scaler(train, test):
     scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
     train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
