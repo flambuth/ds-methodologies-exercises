6085fb2b5f78b9369959d5b04afc30e273924108
Fredrick Lambuth <flambuth@gmail.com>
2019-10-11 09:38:04 -0500

    howdy doody

---
+++ regression/explore.py
@@ -0,0 +1,4 @@
+#exploring the depths of Powershell blue
+
+# Write a function, plot_variable_pairs(dataframe) that plots all of the pairwise 
+# relationships along with the regression line for each pair.
--- regression/split_scale.py
+++ regression/split_scale.py
@@ -119,12 +119,50 @@ test_scaledy = pd.DataFrame(scalery.transform(test), columns=testy.columns.value
 # iqr_robust_scaler()
 
 
+#This function will need to be done first, to generate the train and test DFs that will be given as 
+#parameters for the scaling transformation functions.
 
-def apply_transformation(X,y):
-    scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(train)
+def split_my_data(data, train_ratio=.80, seed=123):
+    '''the function will take a dataframe and returns train and test dataframe split
+    where 80% is in train, and 20% in test. '''
+    return train_test_split(data, train_size = train_ratio, random_state = seed)
 
+
+
+def standard_scaler(train, test):
+    scaler = StandardScaler(copy=True, with_mean=True, with_std=True).fit(train)
     train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
-
     test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
 
-    train_scaled.head()
+
+#This is the one different scaling operation. All the rest do the 1.make scaler, 2.fit scaler, 3.transform process.
+#This one is a do-over scaling operation.
+def my_inv_transform(scaler, train_scaled, test_scaled):
+    train = pd.DataFrame(scaler.inverse_transform(train_scaled), columns=train_scaled.columns.values).set_index([train_scaled.index.values])
+    test = pd.DataFrame(scaler.inverse_transform(test_scaled), columns=test_scaled.columns.values).set_index([test_scaled.index.values])
+    return scaler, train, test
+
+def uniform_scaler(train, test, seed=123):
+    scaler = QuantileTransformer(n_quantiles=100, output_distribution='uniform', random_state=seed, copy=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
+
+def gaussian_scaler(train, test, method='yeo-johnson'):
+    scaler = PowerTransformer(method, standardize=False, copy=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
+
+def my_minmax_scaler(train, test, minmax_range=(0,1)):
+    scaler = MinMaxScaler(copy=True, feature_range=minmax_range).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
+
+def iqr_robust_scaler(train, test):
+    scaler = RobustScaler(quantile_range=(25.0,75.0), copy=True, with_centering=True, with_scaling=True).fit(train)
+    train_scaled = pd.DataFrame(scaler.transform(train), columns=train.columns.values).set_index([train.index.values])
+    test_scaled = pd.DataFrame(scaler.transform(test), columns=test.columns.values).set_index([test.index.values])
+    return scaler, train_scaled, test_scaled
