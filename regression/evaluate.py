9a7a0b0ae3adcd0ad04261135eade583094dc57d
Fredrick Lambuth <flambuth@gmail.com>
2019-10-15 09:38:17 -0500

    darn it

--- regression/evaluate.py
+++ regression/evaluate.py
@@ -54,7 +54,10 @@ def regression_errors(y, yhat):
 
 # Write a function, baseline_mean_errors(y), that takes in your target, y, computes the SSE, MSE & RMSE 
 # when yhat is equal to the mean of all y, and returns the error values (SSE, MSE, and RMSE).
+
 def baseline_mean_errors(y):
+#The yhat in this case is the mean of the target variable. Which is the baseline for predictions. Any model
+#will have to improve on that.    
     yhat = y.mean() 
     df['residual'] = yhat - df['y']
     df['residual^2'] = df.residual ** 2
@@ -63,10 +66,11 @@ def baseline_mean_errors(y):
     RMSE = sqrt(MSE)
     ESS = sum((df.yhat - df.tip.mean())**2)
     TSS = ESS + SSE
-    return SSE, ESS, TSS, MSE, RMSE
+    return SSE, ESS, RMSE
 
 # Write a function, better_than_baseline(SSE), that returns true if your model performs better than the 
 # baseline, otherwise false.
+
 def better_than_baseline(x,y):
     #This will add a column to the dataframe x and y were sliced from.
     make_ols_model(x,y)
@@ -81,5 +85,9 @@ def better_than_baseline(x,y):
 # Write a function, model_significance(ols_model), that takes the ols model as input and returns the amount 
 # of variance explained in your model, and the value telling you whether the correlation between the model 
 # and the tip value are statistically significant.
-def model_significance(ols_model):
-    
+
+
+def model_significance(ols_model):    
+#I didnt get to it, but i know we have to make a T-TEST betwen the predicted y and the mean of y. We want to
+#see a P-value below 0.05 to say that is is "siginficatly different".
+    pass
