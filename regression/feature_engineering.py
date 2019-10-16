cf510114ba1d8fce90fc4e89b250eb1712812405
Fredrick Lambuth <flambuth@gmail.com>
2019-10-16 08:58:47 -0500

    sabrina is so smart

--- regression/feature_engineering.py
+++ regression/feature_engineering.py
@@ -12,6 +12,7 @@ import env
 import wrangle
 import split_scale
 
+import statsmodels.api as sm
 
 # Our scenario continues:
 # As a customer analyst, I want to know who has spent the most money with us over their lifetime. I 
@@ -43,14 +44,66 @@ def select_kbest_freg(X_train, y_train, k_features):
 #3
 # Write a function, ols_backware_elimination() that takes X_train and y_train (scaled) as input and 
 # returns selected features based on the ols backwards elimination method.
-def ols_backware_elimination():
-    pass
 
+#Backwards elimination notes:
+# 1     Select a significance level, say 5% (0.05)
+# 2     Fit a model with all features (variables)
+# 3     Consider the feature with the highest P-Value. If its P-value is greater than significance 
+#       level (P > SL), go to step 4. Else, your model is ready.
+# 4     Eliminate this feature (variable).
+# 5     Fit a model with the new set of features, and go to step 3.
+
+
+#Every step is a model is being 1Instantiated, 2Fitted, 3Transformed. The p-values after each 1-2-3
+#is tested to see if its above 0.05 and worthy of being a not-cut feature
+#Ordinary Least Squares is the ML Algorithm we are testing on each iteration
+
+def ols_backwards_elimination(X_train, y_train):
+    train, test = split_scale.split_my_data(grades_data)
+    scaler, train, test = split_scale.standard_scaler(train, test)
+    X_train = train.drop(columns='final_grade')
+    y_train = train[['final_grade']]
+    X_test = test.drop(columns='final_grade')
+    y_test = test[['final_grade']]
+    cols = list(X_train.columns)
+    pmax = 1
+    while (len(cols)>0):
+    p= []
+    X_1 = X_train[cols]
+    X_1 = sm.add_constant(X_1)
+    model = sm.OLS(y_train,X_1).fit()    
+#Looks like p is an array with each value being hte pvalue of each row.
+#Apparently model objects have a pvalues attribute
+    p = pd.Series(model.pvalues.values[1:],index = cols)
+#Of those pvalues in that series, pmax is the one with the highest p-value. That's the first feature to go!
+    pmax = max(p)
+    feature_with_p_max = p.idxmax()
+#This will iterate through until there is only features that havea p-value above 0.05
+    if(pmax>0.05):
+        cols.remove(feature_with_p_max)
+    else:
+#Once there are no p-values that are larger than 0.05, the loop breaks and we're left with cols that made the cut
+        break
+
+selected_features_BE = cols
+print(selected_features_BE)
 #4
 # Write a function, lasso_cv_coef() that takes X_train and y_train as input and returns the coefficients 
 # for each feature, along with a plot of the features and their weights.
 
 def lasso_cv_coef():
+    from sklearn.linear_model import LassoCV
+
+    reg = LassoCV()
+    reg.fit(X_train, y_train)
+
+    print("Best alpha using built-in LassoCV: %f" % reg.alpha_)
+    print("Best score using built-in LassoCV: %f" %reg.score(X_train,y_train))
+    coef = pd.Series(reg.coef_, index = X_train.columns)
+
+
+    print("Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
+        
     pass
 
 #5
