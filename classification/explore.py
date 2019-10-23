b181bf5d04c6593ff19c1405767bf47f5177684c
Fredrick Lambuth <flambuth@gmail.com>
2019-10-23 14:20:12 -0500

    fixing the explore funcitons

--- classification/explore.py
+++ classification/explore.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 #USE df.nunique()<5 instead of this temp list
 def pick_viable_categories(df):
-    discretes = df.select_dtypes(include='object')
+    discretes = df.select_dtypes(include=['object','int64')
     temp = []
     for column in discretes:
         columnSeriesObj = discretes[column]
@@ -43,3 +43,25 @@ def plot_viable_regressors(target, df):
         sns.violinplot(feature, target, data=df, ax=ax[i], alpha=.5)
         ax[i].set_ylabel('average_rate')
         ax[i].axhline(average_rate, ls='--', color='grey')
+
+def plot_violin(features, target, df):
+    for descrete in df[features].select_dtypes([object,int]).columns.tolist():
+        if df[descrete].nunique() <= 5:
+            for continous in df[features].select_dtypes(float).columns.tolist():
+                sns.violinplot(descrete, continous, hue=target,
+                data=df, split=True, palette=['blue','orange'])
+                plt.title(continous + ' x ' + descrete)
+                plt.ylabel(continous)
+                plt.show()
+
+#This works. Sorta. It makes a long list of violin plots that may or may not be useful. It's inner and outer looping 
+#between the discrete variables with the continous variables.
+def X_plot_violin(target, df):
+    discretes = pick_viable_categories(df)
+    contins = pick_viable_regressors(df)
+    for i in contins:
+        for j in discretes:
+            sns.violinplot(i, j, hue=target, data=df, palette=['blue','orange'])
+            plt.title(i + ' x ' + j)
+            plt.ylabel(i)
+            plt.show()
