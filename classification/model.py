a376a07281c90d7527473f41ac639fb63ff4166f
Fredrick Lambuth <flambuth@gmail.com>
2019-10-25 15:50:53 -0500

    stephen is in the room

--- classification/model.py
+++ classification/model.py
@@ -15,12 +15,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.tree import DecisionTreeClassifier, LogisticRegression, RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
 
 
 #pipeline modules
 import acquire
 import explore
 import prepare
+
+#Acquire and Prep
+df = prepare.prep_titanic()
+df.dropna(inplace=True)
+df.isnull().sum()
+#DATA IS ACQUIRED AND PREPARED.
+#LETS DO THE 4 WAY SPLIT OF X-y, and each of those are split into train and test.
+X=df[['pclass','age','fare','sibsp','parch']]
+y=df[['survived']]
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=.30,random_state=123)
 
 #1
 # Fit the logistic regression classifier to your training sample and transform, 
@@ -146,3 +158,25 @@ y_pred_proba2 = rf2.predict_proba(X_train)
 # What are the differences in the evaluation metrics? Which performs better on your in-sample data? Why?
 class_report2 = classification_report(y_train, y_pred2, output_dict=True)
 class_report2['1']
+    #The accuracy went up using these new parameters, but the precisions and recall seemed to get worse. huh?
+
+##############
+#K Nearest Neighbor
+# Fit a K-Nearest Neighbors classifier to your training sample and transform (i.e. make predictions on the training sample)
+knn = KNeighborsClassifier(n_neighbors=5, weights='uniform')
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_train)
+y_pred_proba = knn.predict_proba(X_train)
+
+
+
+# Evaluate your results using the model score, confusion matrix, and classification report.
+# 
+# Print and clearly label the following: Accuracy, true positive rate, false positive rate, true negative rate, 
+# false negative rate, precision, recall, f1-score, and support.
+# 
+# Run through steps 2-4 setting k to 10
+# 
+# Run through setps 2-4 setting k to 20
+# 
+# What are the differences in the evaluation metrics? Which performs better on your in-sample data? Why?
