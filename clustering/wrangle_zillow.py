de3cd50d99edff50ccab91bb9d9a33fbf59828d9
Fredrick Lambuth <flambuth@gmail.com>
2019-11-04 15:54:44 -0600

    now ready to model some clustering

--- clustering/wrangle_zillow.py
+++ clustering/wrangle_zillow.py
@@ -1,1 +1,26 @@
 #Use acquire and summarize to pump out a 'wrangled' zillow dataframe
+# Wrangling
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+# Exploring
+import scipy.stats as stats
+
+# Visualizing
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import seaborn as sns
+from sklearn.model_selection import learning_curve
+%matplotlib inline
+
+import acquire
+import summarize
+import prepare
+
+df = acquire.get_real_zillow_singles()
+
+wrangled_df = prepare.handle_missing_columns(df, .5)
+wrangled_df = prepare.handle_missing_rows(df, .4)
