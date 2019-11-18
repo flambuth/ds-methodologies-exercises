db609a7f26caf71a9a3798359f23b36b9a216533
Fredrick Lambuth <flambuth@gmail.com>
2019-11-18 12:18:20 -0600

    working ith the OPS on the explore exercises

--- time_series/explore.py
+++ time_series/explore.py
@@ -22,9 +22,6 @@ merged_df = pd.merge(sales, stores, how='inner', left_on = 'store', right_on = '
 df_final = pd.merge(merged_df, items, how='inner', left_on = 'item', right_on = 'item_id')
 df2 = prepare.prep_store_data(df_final)
 
-# Plot a time series decomposition.
-# Create a lag plot (day over day).
-# Run a lag correlation.
 
 # 1
 # Split your data into train and test using the sklearn.model_selection.TimeSeriesSplit method.
@@ -51,11 +48,26 @@ test = df_final['2017':].sale_amount.resample('D').agg(aggregation)
 
 # 3
 # Plot the weekly average & the 7-day moving average. Compare the 2 plots.
+# The weekly average
 train.resample('W').mean().plot(figsize=(12, 4))
 plt.show()
-
-train.rolling(5).mean().plot(figsize=(12, 4))
+# The 7 day rolling average
+train.rolling(7).mean().plot(figsize=(12, 4))
 plt.show()
 
 # 4
 # Plot the daily difference. Observe whether usage seems to vary drastically from day to day or has more of a smooth transition.
+train.diff(periods=1).plot(figsize=(12,4))
+
+# 5
+# Plot a time series decomposition.
+decomp = sm.tsa.seasonal_decompose(train.resample('W').mean(), model='additive')
+
+# 6
+# Create a lag plot (day over day).
+pd.plotting.lag_plot(train.resample('D').mean(), lag=1)
+
+# 7
+# Run a lag correlation.
+df_corr = pd.concat([train.shift(1), train], axis=1)
+df_corr.corr()
