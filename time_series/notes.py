7bf8e4747b7d4a8cdc466bece2495ce92f581a9e
Fredrick Lambuth <flambuth@gmail.com>
2019-11-18 10:04:30 -0600

    exploring times series. eespcially the train-test split

---
+++ time_series/notes.py
@@ -0,0 +1,25 @@
+# import pandas as pd
+# df = pd.read_csv('stores.csv')
+# df.head()
+# df = pd.read_csv('sales.csv')
+# df.head()
+# import prepare
+# df2 = prepare.prep_store_data(df)
+# history
+# sales = pd.read_csv('sales.csv')
+# items = pd.read_csv('items.csv')
+# stores = pd.read_csv('stores.csv')
+# merged_df = pd.merge(sales, stores, how='inner', left_on = 'store', right_on = 'store_id')
+# df_final = pd.merge(merged_df, items, how='inner', left_on = 'item', right_on = 'item_id')
+# df_final.head()
+# df.info()
+# item.info()
+# items.info()
+# merged_df.head()
+# items.head()
+# df_final.info()
+# df2 = prepare.prep_store_data(df_final)
+# df2.info()
+# df2.head()
+# df2["2013-01"]
+# history
