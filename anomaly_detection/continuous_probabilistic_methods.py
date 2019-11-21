a0df28c30db55b42eb36fee9ee297001d64ddde0
Fredrick Lambuth <flambuth@gmail.com>
2019-11-21 16:28:41 -0600

    adding the anomaly directory

---
+++ anomaly_detection/continuous_probabilistic_methods.py
@@ -0,0 +1,11 @@
+#Nastia is better as drumAndbass, rather than her ugly techno sets
+
+import pandas as pd
+
+# 1. Define a function named get_lower_and_upper_bounds that has two arguments
+#       The first argument is a pandas Series. 
+#       The second argument is the multiplier, which should have a default argument of 1.5.
+
+def get_lower_and_upper_bounds(series, multiplier=1.5):
+    
+    pass
