689e12252632ce211b9542d60b3ab8874eb0df27
Fredrick Lambuth <flambuth@gmail.com>
2019-11-22 14:55:37 -0600

    j'avois plus de vine

--- anomaly_detection/discrete_anomaly_detection.py
+++ anomaly_detection/discrete_anomaly_detection.py
@@ -16,3 +16,34 @@ import seaborn as sns
 
 # Use basic probability to identify anomalous request methods. 
 # You will want to make sure the text is normalized in order to reduce the noise.
+
+
+#I'm c and p*ing this from the instructions page
+#Looks a little grandiose for my usual coding
+
+colnames=['ip', 'timestamp', 'request_method', 'status', 'size',
+          'destination', 'request_agent']
+df_orig = pd.read_csv('http://python.zach.lol/access.log',          
+                 engine='python',
+                 header=None,
+                 index_col=False,
+                 names=colnames,
+                 sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+                 na_values='"-"',
+                 usecols=[0, 3, 4, 5, 6, 7, 8]
+)
+
+new = pd.DataFrame([["95.31.18.119", "[21/Apr/2019:10:02:41+0000]", 
+                     "GET /api/v1/items/HTTP/1.1", 200, 1153005, np.nan, 
+                     "python-requests/2.21.0"],
+                    ["95.31.16.121", "[17/Apr/2019:19:36:41+0000]", 
+                     "GET /api/v1/sales?page=79/HTTP/1.1", 301, 1005, np.nan, 
+                     "python-requests/2.21.0"],
+                    ["97.105.15.120", "[18/Apr/2019:19:42:41+0000]", 
+                     "GET /api/v1/sales?page=79/HTTP/1.1", 301, 2560, np.nan, 
+                     "python-requests/2.21.0"],
+                    ["97.105.19.58", "[19/Apr/2019:19:42:41+0000]", 
+                     "GET /api/v1/sales?page=79/HTTP/1.1", 200, 2056327, np.nan, 
+                     "python-requests/2.21.0"]], columns=colnames)
+
+df = df_orig.append(new)
