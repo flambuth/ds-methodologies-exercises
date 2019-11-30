ff0343a3d8537ccd999c284c9bd71048fb3d432a
Fredrick Lambuth <flambuth@gmail.com>
2019-11-30 15:38:35 -0600

    i forgot on wednesday

--- anomaly_detection/discrete_anomaly_detection.py
+++ anomaly_detection/discrete_anomaly_detection.py
@@ -18,8 +18,8 @@ import seaborn as sns
 # You will want to make sure the text is normalized in order to reduce the noise.
 
 
-#I'm c and p*ing this from the instructions page
-#Looks a little grandiose for my usual coding
+#C/R*ed from the instructions page
+
 
 colnames=['ip', 'timestamp', 'request_method', 'status', 'size',
           'destination', 'request_agent']
@@ -47,3 +47,68 @@ new = pd.DataFrame([["95.31.18.119", "[21/Apr/2019:10:02:41+0000]",
                      "python-requests/2.21.0"]], columns=colnames)
 
 df = df_orig.append(new)
+
+colnames=['ip', 'timestamp', 'request_method', 'status', 'size',
+          'destination', 'request_agent']
+
+#Quite a few parameters
+df_orig = pd.read_csv('http://python.zach.lol/access.log',          
+                 engine='python',
+                 header=None,
+                 index_col=False,
+                 names=colnames,
+                 sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+                 na_values='"-"',
+                 usecols=[0, 3, 4, 5, 6, 7, 8]
+)
+
+new = pd.DataFrame([["95.31.18.119", "[21/Apr/2019:10:02:41+0000]", 
+                     "GET /api/v1/items/HTTP/1.1", 200, 1153005, np.nan, 
+                     "python-requests/2.21.0"],
+                    ["95.31.16.121", "[17/Apr/2019:19:36:41+0000]", 
+                     "GET /api/v1/sales?page=79/HTTP/1.1", 301, 1005, np.nan, 
+                     "python-requests/2.21.0"],
+                    ["97.105.15.120", "[18/Apr/2019:19:42:41+0000]", 
+                     "GET /api/v1/sales?page=79/HTTP/1.1", 301, 2560, np.nan, 
+                     "python-requests/2.21.0"],
+                    ["97.105.19.58", "[19/Apr/2019:19:42:41+0000]", 
+                     "GET /api/v1/sales?page=79/HTTP/1.1", 200, 2056327, np.nan, 
+                     "python-requests/2.21.0"]], columns=colnames)
+
+df = df_orig.append(new)
+
+
+df.timestamp = df.timestamp.str.replace(r'(\[|\])', '', regex=True)
+df.timestamp= pd.to_datetime(df.timestamp.str.replace(':', ' ', 1)) 
+df = df.set_index('timestamp')
+
+
+for col in ['request_method', 'request_agent', 'destination']:
+    df[col] = df[col].str.replace('"', '')
+
+df['request_method'] = df.request_method.str.replace(r'\?page=[0-9]+', '', regex=True)
+
+
+ip_df = pd.DataFrame(df.ip.value_counts(dropna=False)).reset_index().\
+                rename(index=str, columns={'index': 'ip', 'ip': 'ip_count'})
+ip_df2 = pd.DataFrame(df.ip.value_counts(dropna=False)/df.ip.count()).reset_index().\
+                rename(index=str, columns={'index': 'ip', 'ip': 'ip_proba'})
+ip_df = ip_df.merge(ip_df2)
+
+
+# see those where rate < 1% 
+ip_df[ip_df.ip_proba < .01]
+
+print(len(ip_df))
+
+print(ip_df.tail(10))
+
+plt.figure(figsize=(12, 4))
+splot = sns.barplot(data=ip_df, x = 'ip', y = 'ip_count', ci = None)
+for p in splot.patches:
+    splot.annotate(format(p.get_height(), '.0f'), 
+                   (p.get_x() + p.get_width() / 2., p.get_height()), 
+                   ha = 'center', va = 'center', xytext = (0, 10), 
+                   textcoords = 'offset points'
+                   )
+    plt.xticks(rotation='vertical')
