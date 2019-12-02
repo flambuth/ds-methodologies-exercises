d42d6222b5f0a9c6d162fa232fc7af2bea7befb3
Fredrick Lambuth <flambuth@gmail.com>
2019-12-02 16:42:13 -0600

    gonna present my anomaly findings

--- anomaly_detection/continuous_probabilistic_methods.py
+++ anomaly_detection/continuous_probabilistic_methods.py
@@ -12,12 +12,8 @@ def get_lower_and_upper_bounds(series, multiplier=1.5):
     iqr = q3-q1
     upper_bounds = q3 + (iqr * multiplier)
     lower_bounds = q1 - (iqr * multiplier) 
-    print(f"Any value in {series.name} {multiplier}x above {upper_bounds} and lower than {lower_bounds} are considered outliers.")
+    print(f"Any value above {upper_bounds} and lower than {lower_bounds} are considered outliers.")
     return upper_bounds, lower_bounds
-
-df = pd.read_csv('lemonade.csv')
-
-#df.Price is all the same value
 
 ####################
 # 1
@@ -25,40 +21,3 @@ df = pd.read_csv('lemonade.csv')
 # column of lemonade.csv, using the multiplier of 1.5. 
 # Do these lower outliers make sense? Which outliers should be kept?
 
-for i in df.select_dtypes('number'): 
-    get_lower_and_upper_bounds(df[i], 3) 
-
-# 2
-# Use the IQR Range Rule and the upper and lower bounds to identify the upper outliers of each column of 
-# lemonade.csv, using the multiplier of 1.5. Do these lower outliers make sense?Which outliers should be kept?
-
-# 3
-# Using the multiplier of 3, IQR Range Rule, and the lower and upper bounds, identify the outliers below the 
-# lower bound in each colum of lemonade.csv. Do these lower outliers make sense?Which outliers should be kept?
-
-for i in df.select_dtypes('number'): 
-    get_lower_and_upper_bounds(df[i],multiplier=3) 
-
-# 4
-# Using the multiplier of 3, IQR Range Rule, and the lower and upper bounds, identify the outliers above the 
-# upper_bound in each colum of lemonade.csv. Do these upper outliers make sense? Which outliers should be kept?
-
-
-
-##########
-#SIGMAS
-
-
-def determine_normality(df,column,n_stds=1):
-    series = df[column]
-    series_std = series.std()
-    series_mean = series.mean()
-    std_dev_range = ((series_mean - n_stds*series_std),(series_mean + n_stds*series_std ))
-    within_n_stds = series[series.between(std_dev_range[0],std_dev_range[1], inclusive = True)]
-    percentage = len(within_n_stds)/len(series)
-    print(f'{percentage} of {series.name} values are within {n_stds} standard deviations from {series_mean}.')
-    # return within_n_stds 
-
-#This will print out the amount of column values that are within one standard deviation from their means.
-for i in df.select_dtypes('number'): 
-    determine_normality(df,i) 
