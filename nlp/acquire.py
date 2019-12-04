7ce0ba8bc5830edee89cbcc1664b800dd47c599a
Fredrick Lambuth <flambuth@gmail.com>
2019-12-04 15:09:11 -0600

    starting the web scraping aquire module

---
+++ nlp/acquire.py
@@ -0,0 +1,18 @@
+#I want to be a cowboy. I'm going to be a cowboy. A cowboy!
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# Codeup Blog Articles
+
+# Scrape the article text from the following pages:
+
+# https://codeup.com/codeups-data-science-career-accelerator-is-here/
+# https://codeup.com/data-science-myths/
+# https://codeup.com/data-science-vs-data-analytics-whats-the-difference/
+# https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/
+# https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/
+
+def get_blog_articles():
+    pass
