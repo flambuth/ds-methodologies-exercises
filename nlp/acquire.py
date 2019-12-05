f298803e41b48c0a361ae79d5928860b185c9bfe
Fredrick Lambuth <flambuth@gmail.com>
2019-12-05 10:21:36 -0600

    refactoring my sloppy code

--- nlp/acquire.py
+++ nlp/acquire.py
@@ -4,21 +4,6 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-# Codeup Blog Articles
-
-# Scrape the article text from the following pages:
-urls = [
-"https://codeup.com/codeups-data-science-career-accelerator-is-here/",
-"https://codeup.com/data-science-myths/",
-"https://codeup.com/data-science-vs-data-analytics-whats-the-difference/",
-"https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/",
-"https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/"
-]
-# #The header will avoid that 403 error
-# url = 'https://codeup.com/codeups-data-science-career-accelerator-is-here/'
-# headers = {'User-Agent': 'Codeup Bayes Data Science'} # codeup.com doesn't like our default user-agent
-# response = get(url, headers=headers)
-
 def get_codeup_webcontent(url):
     """
     Returns a 2Key Dictionary that is a string for ['Title'] and a list for ['Content']
@@ -30,7 +15,7 @@ def get_codeup_webcontent(url):
     for i in sopa.find_all('p'):
         #print(i.get_text())
         content.append(i.get_text())
-    title = sopa.select('title')[0].get_text()
+    title = sopa.title.get_text()
     a = {'Title': title, 'Content': content}
     return a
 
--- nlp/scratch.py
+++ nlp/scratch.py
@@ -62,3 +62,18 @@ sopa.find_all('p')
 #THE GOLDEN GOOSE!
 for i in sopa.find_all('p'): 
     ...:     print(i.get_text()) 
+
+    # Codeup Blog Articles
+
+# Scrape the article text from the following pages:
+urls = [
+"https://codeup.com/codeups-data-science-career-accelerator-is-here/",
+"https://codeup.com/data-science-myths/",
+"https://codeup.com/data-science-vs-data-analytics-whats-the-difference/",
+"https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/",
+"https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/"
+]
+# #The header will avoid that 403 error
+# url = 'https://codeup.com/codeups-data-science-career-accelerator-is-here/'
+# headers = {'User-Agent': 'Codeup Bayes Data Science'} # codeup.com doesn't like our default user-agent
+# response = get(url, headers=headers)
