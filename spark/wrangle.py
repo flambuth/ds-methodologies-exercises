3db9c688fefda5fb35946850fe81a1a6ecebfeea
Fredrick Lambuth <flambuth@gmail.com>
2019-12-19 14:16:42 -0600

    working on my spark exploration. no visualizations so far. maybe i should make some

--- spark/wrangle.py
+++ spark/wrangle.py
@@ -15,7 +15,7 @@ from pyspark.sql.functions import (
 
 def get_311_data(spark: pyspark.sql.SparkSession) -> pyspark.sql.DataFrame:
     print("[wrangle.py] reading case.csv")
-    df = spark.read.csv("data/case.csv", header=True, inferSchema=True)
+    df = spark.read.csv("case.csv", header=True, inferSchema=True)
     return df.withColumnRenamed("SLA_due_date", "case_due_date")
 
 
@@ -63,7 +63,7 @@ def join_departments(
     case_df: pyspark.sql.DataFrame, spark: pyspark.sql.SparkSession
 ) -> pyspark.sql.DataFrame:
     print("[wrangle.py] joining departments")
-    dept = spark.read.csv("data/dept.csv", header=True, inferSchema=True)
+    dept = spark.read.csv("dept.csv", header=True, inferSchema=True)
     return (
         case_df.join(dept, "dept_division", "left")
         # drop all the columns except for standardized name, as it has much fewer unique values
